1664528e0584db442b72871cb2848982d9565d8e
CRPPL70 <p.witek70@gmail.com>
2026-09-13 17:38:06 +0200

    added correlation matrix

--- titanic.py
+++ titanic.py
@@ -1,3 +1,4 @@
+# --- DATASET ---
 # import kagglehub
 
 # # Download latest version
@@ -5,4 +6,21 @@
 
 # print("Path to dataset files:", path)
 
-print("Hello World!")
+# --- IMPORTS ---
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns # Helps with drawing correlation matrix
+
+df = pd.read_csv("Titanic-Dataset.csv")
+
+df = df.drop(['Name','Ticket','Cabin', 'Embarked'], axis=1)
+df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
+
+# Compute correlation matrix
+correlation_matrix = df.corr()
+
+# Drawing a heatmap
+plt.figure(figsize=(12, 10))
+sns.heatmap(correlation_matrix, annot=False, cmap='coolwarm')
+plt.title('Working conditions and mental health correlation matrix')
+plt.show()
